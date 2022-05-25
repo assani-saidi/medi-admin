06078c2a0b41ad2e53f7a1f1416dbd2a9ac3cf03
assani-saidi <assanisaidi73@gmail.com>
2022-05-25 15:49:27 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   BACKEND_URL: "https://medi-identity-django-backend-git-assanisaidi73-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/api/",
-  IO_URL: "http://192.168.43.42:4000",
+  IO_URL: "https://socket-medi-backend-git-assanisaidi73-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/",
 };
